c5fafbf8f4a78a33d9298c090cce2ecd5d6c13a2
Ethan <ebonsignori@gmail.com>
2019-03-17 17:07:31 -0400

    file structure and basic html in place

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//Possible features: Bar display on timer instead of number only, random order to questions and answers,
+// select length of game (# of questions), sound effects, gifs 
+
+// Start button to start the timer / game
+
+// Show question and start timer
+
+// On guess, pause timer, show image and correct answer if wrong, show correct if correct
+// Count down for about 5 seconds and display next question
+
+// At end show correct/incorrect answers and start over button
