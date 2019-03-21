a05bdf8f2a0a31c80ec702c2fcf6b0580fa5e058
Ethan <ebonsignori@gmail.com>
2019-03-21 17:12:40 -0400

    timer bar working, gif display working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,8 @@
 // select length of game (# of questions), sound effects, gifs 
 // Harder difficulty - less time to answer questions
 
-let title, subtitle, startButton, content, tab, chosenCategory, shuffledQuestions, questionCounter, questionText,
+let title, subtitle, startButton, content, tab, chosenCategory, questionTime, 
+shuffledQuestions, questionCounter, questionText,
 qIndex, choice, correctAnswers, incorrectAnswers, totalCorrectAnswers, totalIncorrectAnswers;
 
 correctAnswers = 0;
@@ -10,35 +11,50 @@ incorrectAnswers = 0;
 totalCorrectAnswers = 0;
 totalIncorrectAnswers = 0;
 
+let diffEasy = 30;
+let diffMedium = 20; 
+let diffHard = 10;
+let diffChosen = diffMedium;
+
 let categories = [
 
    animals = {
     name: 'Animals',
     tabId: 'animals',
     value: 1,
-    length: 4,
+    length: 5,
     difficulty: 'Easy',
     description: 'The animal category contains questions dealing with everything in the animal kingdom!',
     questions: [
       { 
         question: "What is the name for a collective group of lions?",
         answer: "Pride",
-        choices: ["Pride", "Flock", "Herd", "Group"]
+        choices: ["Pride", "Flock", "Herd", "Group"],
+        gif: "12Iabt4Yt9hSUM",
       },
       { 
         question: "What type of animal is a Portugeuse man o' war?",
         answer: "Jellyfish",
-        choices: ["Jellyfish", "Snail", "Dolphin", "It's not an animal"]
+        choices: ["Jellyfish", "Snail", "Dolphin", "It's not an animal"],
+        gif: "mlYRUYfJKpINW",
       },
       { 
         question: "What is the natural habitat of arboreal animals?",
         answer: "In the trees",
-        choices: ["In the trees", "Under the desert sand", "In the deep ocean", "Your attic"]
+        choices: ["In the trees", "Under the desert sand", "In the deep ocean", "Your attic"],
+        gif: "XBbe8ApwcblsY",
       },
       { 
         question: "What is the largest living bird?",
         answer: "Ostrich",
-        choices: ["Ostrich", "Bald Eagle", "Condor", "Pterodactyl"]
+        choices: ["Ostrich", "Bald Eagle", "Condor", "Pterodactyl"],
+        gif: "vFXGVzIlemGu4",
+      },
+      { 
+        question: "This animal is considered the deadliest animal ever, excluding humans.",
+        answer: "Mosquito",
+        choices: ["Mosquito", "Great-White Shark", "Hippopotamus", "Dog"],
+        gif: "l0MYsIQmIVdCG6w4o",
       },
     ],
 
@@ -48,29 +64,45 @@ let categories = [
     name: 'Science',
     tabId: 'science',
     value: 2,
-    length: 4,
+    length: 6,
     difficulty: 'Medium',
     description: 'The science category contains questions that are sure to baffle the mind with a wide variety of science related questions ranging from biology to physics!',
     questions: [
       { 
         question: "What is the opposite of matter?",
         answer: "Antimatter",
-        choices: ["Antimatter", "Dark Matter", "A Black Hole", "Inverse Matter"]
+        choices: ["Antimatter", "Dark Matter", "A Black Hole", "Inverse Matter"],
+        gif: "8JMW0KHcdqrq8"
       },
       { 
-        question: "People with which blood type are considered 'universal donors'?",
+        question: "People with this blood type are considered 'universal donors'.",
         answer: "O type",
-        choices: ["O type", "AB type", "A type", "B type"]
+        choices: ["O type", "AB type", "A type", "B type"],
+        gif: "xUA7biF7n8LcAqh4bK"
       },
       { 
         question: "When eating a hot pepper, what substance causes the burning sensation?",
         answer: "Capsaicin",
-        choices: ["Capsaicin", "Gingerol", "Allyl isothiocyanate", "Vitamin C"]
+        choices: ["Capsaicin", "Gingerol", "Allyl isothiocyanate", "Vitamin C"],
+        gif: "5hoaXceZ1w1CNtd1N3"
       },
       { 
         question: "Which planet in our solar system is closest to the sun?",
         answer: "Mercury",
-        choices: ["Mercury", "Earth", "Venus", "Uranus"]
+        choices: ["Mercury", "Earth", "Venus", "Uranus"],
+        gif: "l4FGJQvXJdKsBboha"
+      },
+      { 
+        question: "In 1633 the Church condemned him to house arrest for astronomical heresy.",
+        answer: "Galileo",
+        choices: ["Galileo", "Sir Isaac Newton", "Johann Sebastian Bach", "John Locke"],
+        gif: "l2Sqd3jnE4QEyOPM4"
+      },
+      {
+        question: "It's the more common name for sodium chloride.",
+        answer: "Salt",
+        choices: ["Salt", "Bleach", "Water", "Toothpaste"],
+        gif: "QOgvV9rV4hHpgNRBfQ"
       },
     ],
     
@@ -80,7 +112,7 @@ let categories = [
     name: 'Potent Potables',
     tabId: 'potent-potables',
     value: 3,
-    length: 4,
+    length: 8,
     difficulty: 'Hard',
     description: 'A trivia category popularized by the long running TV show Jeopardy! and several appearances on the Saturday Night Live\'s skit Celebrity Jeopardy!' +
     ' This category features questions having to do with alcoholic beverages.',
@@ -88,23 +120,51 @@ let categories = [
       { 
         question: "More similar to beer than wine in production, this alcohol begins with steamed rice & mold.",
         answer: "Sake",
-        choices: ["Sake", "Tequila", "Vodka", "Everclear"]
+        choices: ["Sake", "Tequila", "Vodka", "Everclear"],
+        gif: "xUPGcsiNHoFdtXWG1q"
       },
       { 
         question: "The brand called Mike's Hard makes cider & punch but its logo shows this: its flagship drink.",
         answer: "Lemonade",
-        choices: ["Lemonade", "Juice", "Club Soda", "Water"]
+        choices: ["Lemonade", "Juice", "Club Soda", "Water"],
+        gif: "Mv3QingEh0SaI"
       },
       { 
-        question: "The name of this spirit comes from the Russian word for water",
+        question: "The name of this spirit comes from the Russian word for 'water.'",
         answer: "Vodka",
-        choices: ["Vodka", "Rum", "Gin", "Absinthe"]
+        choices: ["Vodka", "Rum", "Gin", "Absinthe"],
+        gif: "iEBXec8t3zod2"
       },
       { 
-        question: "The flowers of this plant give beer its bitter taste",
+        question: "The flowers of this plant give beer its bitter taste.",
         answer: "Hops",
-        choices: ["Hops", "Chamomile", "Basil", "Yarrow"]
+        choices: ["Hops", "Chamomile", "Basil", "Yarrow"],
+        gif: "3HwNaSR0VRk2Z6CTgM"
       },
+      {
+        question: "What is the official distilled spirit of the United States?",
+        answer: "Bourbon",
+        choices: ["Bourbon", "Rum", "Moonshine", "Tequila"],
+        gif: "O9vkkgKfHBDhu"
+      },
+      {
+        question: "This brand has been made at the Lynchburg, Tennessee distillery since 1866.",
+        answer: "Jack Daniels",
+        choices: ["Jack Daniels", "Jim Beam", "Hennessy", "Maker's Mark"],
+        gif: "2w5UtbNhIyP3G"
+      },
+      {
+        question: "Peppermint & root beer are popular flavors of this strong potable, from the German for 'mouthful.'",
+        answer: "Schnapps",
+        choices: ["Schnapps", "Absolut", "Jagermeister", "Patrón"],
+        gif: "9lCrsRjrg9MI0"
+      },
+      {
+        question: "It puts the fizz in a gin fizz.",
+        answer: "Club Soda",
+        choices: ["Club Soda", "Pineapple Juice", "Coca-Cola", "Alka-Seltzer"],
+        gif: "37QGsYyVuSKZdhd2IF"
+      },  
     ],
     
   },
@@ -165,8 +225,11 @@ selectCategoryHtml = () => {
   categories.forEach( function( i ) {
     // Create buttons
     tab.append( 
-      `<button type="button" class="list-group-item btn-category tablinks" value="${ i.value }"  
-      id="cat-btn-${i.value} "onmouseover="openCategory(event, '${ i.tabId }')">${ i.name }</button></div>`
+      `<button type="button" class="list-group-item btn-category tablinks" value="${ i.value }"   
+      id="cat-btn-${i.value}"
+      onmouseover="openCategory( event, '${ i.tabId }' )" 
+      onclick="questionTimer()">
+      ${ i.name }</button></div>`
     )
     // Create tab content to show on hover
     jumbotron.append(
@@ -179,51 +242,90 @@ selectCategoryHtml = () => {
     )
   } );
 
+
+
   // Get player's chosen category
   $( '.btn-category' ).on( 'click', function() {
-    chosenCategory = this.value;
+    chosenValue = this.value;
     for ( let i = 0; i < categories.length; i++ ) {
-      if ( categories[i].value == chosenCategory ) {
-        startGame( categories[i] );
+      if ( categories[i].value == chosenValue ) {
+        chosenCategory = categories[i];
+        startGame( chosenCategory );
       };
     };
   } );
 
 };
 
-startGame = ( cat ) => {
+startGame = ( category ) => {
   questionCounter = 0;
   questionCounterHtml.text( `Question ${questionCounter + 1}` ).hide();
   // Switch subtitle to category name and display question counter
   subtitle.fadeOut( 0, function() {
-    subtitle.text( cat.name ).fadeIn( 200 );
+    subtitle.text( category.name ).fadeIn( 200 );
     questionCounterHtml.fadeIn( 200 );
       } );
   // Remove all select categories html
   removeCategories();
   // Randomize order of questions
-  shuffledQuestions = shuffle( [...cat.questions] );
+  shuffledQuestions = shuffle( [...category.questions] );
   content.append( `<h4 id="question"><b></b></h4>` ); // Create an element for our question to be displayed
   questionText = $( '#question' );                    // Store the element for later use
   displayQuestion( shuffledQuestions );     
-  
-  // Console log for testing purposes
-  testRandom( shuffledQuestions );
 }
 
 // Find index of current question and display to screen
-displayQuestion = ( q ) => {
-  qIndex = (q.length - q.length) + questionCounter;
-  console.log("question index: " + qIndex)
-  questionText.text( q[qIndex].question );
+displayQuestion = ( qs ) => {
+  qIndex = questionCounter;
+  questionText.text( qs[qIndex].question );
   questionCounter++; // Increment question counter so a new question is displayed on every call
-  generateAnswerHtml( q );
+  getChoices( qs );
 }
 
 // Generates html to display each possible answer in a list
-generateAnswerHtml = ( a ) => {
+getChoices = ( q ) => {
+  generateChoiceHtml( q );
+  choice = $( '.choice' )
+  choice.on( 'click', function() {
+    guess = $( this ).text().substring( 3 );
+    if ( guess === q[qIndex].answer) {
+      correctAnswer( q );
+    } else {
+      incorrectAnswer( q );
+    }
+    $( '.list-group' ).remove();
+    clearInterval(questionIntervalId);
+    clearTimeout(questionTimerId);
+  } );
+};
+
+
+questionTime = 1000 * diffChosen; 
+questionTimer = () => {
+  questionIntervalId = setInterval( updateTimerBar, 1000 );
+  questionTimerId = setTimeout( function() {
+    timeUp() 
+  }, questionTime )
+}
+
+
+
+let barWidth = 100;
+let seconds = diffChosen;
+let percent = barWidth / seconds;
+updateTimerBar = () => {
+  barWidth -= percent;
+  remainingTime = seconds -= 1;
+  document.getElementById( 'timer-bar' ).style.width = `${barWidth}%`;
+  if ( remainingTime >= 0 ) {
+    document.getElementById( 'time-remaining' ).innerHTML = `${remainingTime}`;
+  };
+} 
+
+
+generateChoiceHtml = ( q ) => {
   // Randomize order of choices and store in array 
-  let choiceOrder = shuffle( [...a[qIndex].choices] );
+  let choiceOrder = shuffle( [...q[qIndex].choices] );
   // Create element to append our choices
   content.append( `<div class="list-group"></div>` );
   // Loop to create each choice element, every question has 4 choices so this will work on every question
@@ -233,30 +335,42 @@ generateAnswerHtml = ( a ) => {
       `<button type="button" class="list-group-item list-group-item-action choice">${listNum}. ${choiceOrder[i]}</button>`
     );
   };
-
-  choice = $( '.choice' )
-  choice.on( 'click', function() {
-    guess = $( this ).text().substring( 3 );
-    if ( guess === a[qIndex].answer) {
-      correctAnswers++;
-    } else {
-      incorrectAnswers++;
-    }
-    questionCounter++;
-    console.log(`Correct answers: ${correctAnswers}, Incorrect answers: ${incorrectAnswers}`)
-    $( '.list-group' ).empty();
-    displayQuestion( a );
-  } );
-};
-
-
-
-// Remove all select categories html
-removeCategories = () => {
-  tab.remove();
-  $( '.tabcontent' ).remove();
+  // Create element to display our timer
+  $( '.list-group' ).append( `
+  <div class="progress">
+    <div class="progress-bar" id="timer-bar" role="progressbar" style="width: 100%;">
+      <p id="time-remaining">${questionTime / 1000}</p>
+    </div>
+  </div>` )
 }
 
+correctAnswer = ( q ) => {
+  correctAnswers++;
+  questionCounter++;
+  console.log( `Correct answer. Total: ${correctAnswers}` )
+  questionText.text( `Correct!` )
+  showGif();
+}
+
+incorrectAnswer = ( q ) => {
+  incorrectAnswers++;
+  questionCounter++;
+  questionText.html( `Incorrect! The correct answer was: <b>${shuffledQuestions[0].answer}</b>` )
+  console.log( `Incorrect answer. Total: ${incorrectAnswers}`)
+  showGif();
+}
+
+timeUp = () => {
+  questionCounter++;
+  console.log("Time's up")
+  $( '.list-group' ).remove();
+  questionText.html( `You ran out of time! The correct answer was: <b>${shuffledQuestions[0].answer}</b>` )
+  showGif();
+}
+
+showGif = () => {
+  content.append( `<div id="gif"><iframe src="https://giphy.com/embed/${shuffledQuestions[qIndex].gif}" width="480" height="480" frameBorder="0" class="giphy-embed"></iframe></div>` )
+}
 // Randomize the order of input array so questions/choices appear in a different order on each game
 // Fisher-Yates Shuffle
 shuffle = ( array ) => {
@@ -277,9 +391,6 @@ shuffle = ( array ) => {
   return array;
 }
 
-testRandom = (x) => {
-  console.log( "Shuffled questions array: " + JSON.stringify(x) )
-}
 
 // Tabs for categories on mouseover
 openCategory = ( event, categoryName ) => {
@@ -303,33 +414,8 @@ openCategory = ( event, categoryName ) => {
   event.currentTarget.className += " active";
 }
 
-
-// Timer for questions
-questionTimer = () => { 
-  $("#countdown").countdown360({
-    radius      : 60.5,
-    seconds     : 30,
-    strokeWidth : 15,
-    fillStyle   : '#0276FD',
-    strokeStyle : '#003F87',
-    fontSize    : 50,
-    fontColor   : '#FFFFFF',
-
-    autostart: false,
-    onComplete  : function () { console.log('completed') }
-  }).start()
-  
-};
-
-
-// Start button to start the timer / game
-
-// Append title, timer and divs, 
-
-// Show question and start timer
-
-// On guess, pause timer, show image and correct answer if wrong, show correct if correct
-// Count down for about 5 seconds and display next question
-
-// At end show correct/incorrect answers and start over button
-
+// Remove all select categories html
+removeCategories = () => {
+  tab.remove();
+  $( '.tabcontent' ).remove();
+}
